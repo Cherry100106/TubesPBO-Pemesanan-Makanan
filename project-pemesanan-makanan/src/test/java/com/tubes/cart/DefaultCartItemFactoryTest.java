0503cba9dfc1ae82a6f9e3667cc70505bbfdb15c
byebay <bayuuua00@gmail.com>
2025-11-26 10:39:12 +0700

    implementasi Unit Testing

---
+++ project-pemesanan-makanan/src/test/java/com/tubes/cart/DefaultCartItemFactoryTest.java
@@ -0,0 +1,23 @@
+package com.tubes.cart;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import org.junit.jupiter.api.Test;
+
+import com.tubes.menu.MenuItem;
+
+public class DefaultCartItemFactoryTest {
+
+    @Test
+    public void factoryCreatesCorrectCartItem() {
+        MenuItem menu = new MenuItem(10, "Ayam Bakar", "Makanan", 30000, true);
+        DefaultCartItemFactory factory = new DefaultCartItemFactory();
+
+        CartItem item = factory.create(menu, 2);
+
+        assertNotNull(item);
+        assertEquals(menu, item.getMenu());
+        assertEquals(2, item.getQuantity());
+        assertEquals(60000.0, item.getSubtotal(), 0.0001);
+    }
+}
