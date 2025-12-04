3015f7579eab7b00f8d4ef05d868abfe1fe09846
Farell Daffa Aditya <farelldaffaaditya100106@gmail.com>
2025-12-04 21:29:04 +0700

    Mengganti komentar

--- project-pemesanan-makanan/src/main/java/com/tubes/db/Koneksi.java
+++ project-pemesanan-makanan/src/main/java/com/tubes/db/Koneksi.java
@@ -7,14 +7,13 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
  @SuppressWarnings("java:S6437") 
 public class Koneksi {
-    // Ubah sesuai konfigurasi database Anda
+    // GANTI 'tubes_pbo' dengan nama database masing masing
     private static final String URL = "jdbc:postgresql://localhost:5432/PemesananMakanan";
     private static final String USER = "postgres";
-    private static final String PASS = "1234"; // Sesuaikan password, JANGAN gunakan password default di produksi!
+    private static final String PASS = "1234"; // Sesuaikan password
 
     private static final Logger logger = Logger.getLogger(Koneksi.class.getName());
 
-    // Utility class: hide constructor
     private Koneksi() {}
 
     public static Connection getConnection() {
